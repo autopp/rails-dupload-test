d6f2be59b91a477796d30387a39725397965a82d
autopp <autopp.inc@gmail.com>
2020-09-05 23:17:49 +0900

    feat: impl POST api/users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :direct_uploads, only: [:create]
+    resources :users, only: [:create]
   end
   resources :users
   root to: 'home#index'
